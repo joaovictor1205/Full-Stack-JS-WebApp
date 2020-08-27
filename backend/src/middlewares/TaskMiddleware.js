67d96b65d85f255c96841e1669df19c867582b8a
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-08-27 22:37:45 +0000

    Date validation

--- backend/src/middlewares/TaskMiddleware.js
+++ backend/src/middlewares/TaskMiddleware.js
@@ -18,6 +18,18 @@ const TaskMiddleware = async(req, res, next) => {
     } else if (isPast(new Date(when))){
         return res.status(400).json({ error: 'Data inválida!' });
     } else {
+
+        let exists;
+
+        exists = await TaskModel.findOne({
+            'when': {'$eq': new Date(when)},
+            'macAddress': {'$in': macAddress},
+        });
+
+        if(exists){
+            return res.status(400).json({ error: 'Já existe uma tarefa cadastrada neste horário!' });
+        }
+
         next();
     }
 
