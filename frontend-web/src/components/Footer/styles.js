0a63f30dcc164d733c4b9294e12afb928dd94db6
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-08-29 20:15:30 +0000

    Making Footer Component

---
+++ frontend-web/src/components/Footer/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 50px;
+
+    background: #20295F;
+    border-top: 5px solid #EE6B26;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: fixed;
+    bottom: 0;
+
+    span{
+        color: white;
+    }
+`
--- frontend-web/src/views/Home/index.js
+++ frontend-web/src/views/Home/index.js
@@ -3,11 +3,14 @@ import * as S from './styles';
 
 // Components Import
 import Header from '../../components/Header/index';
+import Footer from '../../components/Footer/index';
 
 function Home() {
   return (
     <S.Container>
       <Header />
+
+      <Footer />
     </S.Container>
   );
 }
