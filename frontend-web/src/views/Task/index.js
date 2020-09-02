15bb057981f9ab045bf1637f114aafc2fc056357
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-09-02 14:55:13 +0000

    POST Task

--- frontend-web/src/views/Task/index.js
+++ frontend-web/src/views/Task/index.js
@@ -13,12 +13,31 @@ function Task() {
 
     const [ lateCount, setLateCount ] = useState();
     const [ type, setType ] = useState();
+    const [ id, setId ] = useState();
+    const [ done, setDone ] = useState(false);
+    const [ title, setTitle ] = useState();
+    const [ description, setDescription ] = useState();
+    const [ date, setDate ] = useState();
+    const [ hour, setHour ] = useState();
+    const [ macAddress, setMacAddress ] = useState('11:11:11:11:11:11');
 
     async function lateVerify(){
         await api.get(`task/filter/late/11:11:11:11:11:11`)
         .then(response => {
             setLateCount(response.data.length);
         });
+    }
+
+    async function save(){
+        await api.post('/task', {
+            macAddress,
+            type,
+            title,
+            description,
+            when: `${date}T${hour}:00.000`
+        }).then( () =>
+            alert('Tarefa cadastrada')
+        );
     }
 
     useEffect( () => {
@@ -46,27 +65,49 @@ function Task() {
 
                 <S.Input>
                     <span> Título </span>
-                    <input type="text" placeholder="Título da tarefa"></input>
+                    <input
+                        type="text"
+                        placeholder="Título da tarefa"
+                        onChange={ e => setTitle(e.target.value) }
+                        value={ title }
+                    />
                 </S.Input>
 
                 <S.TextArea>
                     <span> Descrição </span>
-                    <textarea rows={5} placelholder="Detalhes da tarefa"></textarea>
+                    <textarea
+                        rows={5}
+                        placelholder="Detalhes da tarefa"
+                        onChange={ e => setDescription(e.target.value) }
+                        value={ description }
+                    />
                 </S.TextArea>
 
                 <S.Input>
                     <span> Data </span>
-                    <input type="date" placeholder="Título da tarefa"></input>
+                    <input
+                        type="date"
+                        onChange={ e => setDate(e.target.value) }
+                        value={ date }
+                    />
                 </S.Input>
 
                 <S.Input>
                     <span> Hora </span>
-                    <input type="time" placeholder="Título da tarefa"></input>
+                    <input
+                        type="time"
+                        onChange={ e => setHour(e.target.value) }
+                        value={ hour }
+                    />
                 </S.Input>
 
                 <S.Options>
                     <div>
-                        <input type="checkbox"></input>
+                        <input
+                            type="checkbox"
+                            checked={ done }
+                            onChange={ () => setDone(!done) }
+                        />
                         <span>CONCLUÍDO</span>
                     </div>
 
@@ -74,7 +115,7 @@ function Task() {
                 </S.Options>
 
                 <S.Save>
-                    <button type="button">
+                    <button type="button" onClick={save}>
                         Salvar
                     </button>
                 </S.Save>
